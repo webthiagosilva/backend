b43200f5af55bc4be0ffd063e82dc3e4ae0e9780
Thiago Silva <webthiagosilva@gmail.com>
2023-02-04 00:48:17 -0300

    feat(api-auth): customize exception rendering according to http standards

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,17 @@
 
 namespace App\Exceptions;
 
+use App\Support\LogErrorCode;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -22,7 +32,11 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        //
+        AuthenticationException::class,
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -46,5 +60,80 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request $request
+     * @param Throwable $exception
+     * @return JsonResponse|Response|\Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        $data = [
+            'message' => $this->getErrorMessage($exception),
+            'track_id' => strtoupper(uniqid()),
+            'error_code' => null,
+        ];
+
+        switch (true) {
+            case $exception instanceof HttpException:
+                $statusCode = $exception->getStatusCode();
+                break;
+
+            case $exception instanceof ModelNotFoundException:
+                $statusCode = Response::HTTP_NOT_FOUND;
+                break;
+
+            case $exception instanceof AuthenticationException:
+                $statusCode = Response::HTTP_UNAUTHORIZED;
+                break;
+
+            case $exception instanceof ValidationException:
+                $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY;
+                $data['errors'] = $exception->errors();
+                break;
+
+            case $exception instanceof AuthorizationException:
+                $statusCode = Response::HTTP_FORBIDDEN;
+                break;
+        }
+
+        $data['error_code'] = LogErrorCode::getErrorCodeIdentifier($statusCode);
+
+        Log::debug('Exception', ['data' => array_merge($data, [
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+        ])]);
+
+        return apiResponse($data, $statusCode, false);
+    }
+
+    /**
+     * Get error message from exception.
+     *
+     * @param Throwable $exception
+     * @return string
+     */
+    private function getErrorMessage(Throwable $exception): string
+    {
+        switch (true) {
+            case $exception instanceof HttpException:
+                return $exception->getMessage();
+            case $exception instanceof ModelNotFoundException:
+                return 'The requested resource was not found';
+            case $exception instanceof AuthenticationException:
+                return 'Unauthorized access';
+            case $exception instanceof ValidationException:
+                return 'Validation error';
+            case $exception instanceof AuthorizationException:
+                return 'This action is unauthorized';
+            default:
+                //return 'Internal Server Error';
+                return $exception->getMessage();
+        }
     }
 }
