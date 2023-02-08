a341f4a2c1fe56edf68f337050e6b2fbe6b30f16
Thiago Silva <webthiagosilva@gmail.com>
2023-02-08 17:13:03 -0300

    feat(api-auth): add name to routes

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,14 @@ use App\Http\Controllers\Auth\AuthController;
 */
 
 Route::middleware('api')->group(function () {
-    Route::group(['prefix' => 'auth'], function () {
-        Route::post('/register', [RegisterController::class, 'register']);
-        Route::post('/login', [LoginController::class, 'login']);
+    Route::group([
+        'prefix' => 'auth'
+    ], function () {
+        Route::post('/register', [RegisterController::class, 'register'])->name('register');
+        Route::post('/login', [LoginController::class, 'login'])->name('login');
 
         Route::middleware('auth:sanctum')->group(function () {
-            Route::post('/logout', [AuthController::class, 'logout']);
+            Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
         });
     });
 });
