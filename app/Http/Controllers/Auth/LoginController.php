3ab260b7d2df7636c8f854b6cff7787e2b2fa24a
Thiago Silva <webthiagosilva@gmail.com>
2023-02-07 22:09:22 -0300

    feat(api-auth): add login controller and access token generation function and resouce for doc

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserLoginRequest;
+use Carbon\Carbon;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class LoginController extends Controller
+{
+    /**
+     * Generate a new access Login.
+     *
+     * @bodyParam email string required The email of the user.
+     * @bodyParam password string required The password of the user.
+     *
+     * @responseFile responses/login.post.json
+     *
+     * @param  \App\Http\Requests\Auth\UserLoginRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(UserLoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!auth()->attempt($credentials)) {
+            throw new UnauthorizedHttpException('Invalid credentials');
+        }
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+
+        $token = $user->createToken(
+            'auth_token',
+            ['*'],
+            Carbon::now()->addMinutes(config('sanctum.expiration'))
+        );
+
+        return response()->json([
+            'success' => true,
+            'message' => __('auth.token.success'),
+            'data' => [
+                'access_token' => $token->plainTextToken,
+                'token_type' => 'Bearer',
+            ],
+        ], Response::HTTP_OK);
+    }
+}
